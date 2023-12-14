688bb5243c4fe6930dae0cd8611cc7cc6af5706b
Abhishek Aryal <abiary8@gmail.com>
2023-12-14 13:49:50 +0545

    first commit

---
+++ health_monitoring_system/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:live_graph_plotter/pages/nav_bar_page.dart';
+import 'views/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  User? user;
+  //If user is logged in then it wont be null otherwise it will be null so we have used nullable User? user
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: (user != null) ? const NavBarPage() : const UserLoginPage(),
+      // home: UserLoginPage(),
+    );
+  }
+}
